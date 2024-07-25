885753a52584a7426d0ac884ee44d9fca187c9eb
JuanCruzRusconi <juanrusconi@hotmail.es>
2024-07-25 20:20:18 -0300

    changes

--- src/Components/IconsMenu/IconsMenu.jsx
+++ src/Components/IconsMenu/IconsMenu.jsx
@@ -1,14 +1,15 @@
-import htmlIcon from "/svgs/html5-solid.svg";
-import cssIcon from "/svgs/css3-solid.svg";
-import javascriptIcon from "/svgs/js-solid.svg";
-import reactIcon from "/svgs/react-solid.svg";
-import mongodbIcon from "/svgs/mongodb-solid.svg";
-import expressIcon from "/svgs/express-solid.svg";
-import nodeIcon from "/svgs/node-solid.svg";
-import firebaseIcon from "/svgs/firebase-solid.svg";
-import dockerIcon from "/svgs/docker-solid.svg";
-import typescriptIcon from "/svgs/typescript-solid.svg"
-import codeIcon from "/svgs/code-solid.svg";
+import codeOpenIcon from "/svgs/code-open-iconsMenu.svg";
+import htmlIcon from "/svgs/html5-iconsMenu.svg";
+import cssIcon from "/svgs/css3-iconsMenu.svg";
+import javascriptIcon from "/svgs/javascript-iconsMenu.svg";
+import typescriptIcon from "/svgs/typescript-iconsMenu.svg"
+import reactIcon from "/svgs/react-iconsMenu.svg";
+import mongodbIcon from "/svgs/mongodb-iconsMenu.svg";
+import expressIcon from "/svgs/express-iconsMenu.svg";
+import nodeIcon from "/svgs/node-iconsMenu.svg";
+import firebaseIcon from "/svgs/firebase-iconsMenu.svg";
+
+import codeCloseIcon from "/svgs/code-close-iconsMenu.svg";
 
 import "./IconsMenu.css";
 
@@ -17,18 +18,17 @@ const IconsMenu = () => {
     return (
 
         <div className="icons-menu-container">
-            <img className="icon-item" src={codeIcon} alt="code" />
+            <img className="icon-item" src={codeOpenIcon} alt="code" />
             <img className="icon-item" src={htmlIcon} alt="html" />
             <img className="icon-item" src={cssIcon} alt="css" />
             <img className="icon-item" src={javascriptIcon} alt="javascript" />
-            <img className="icon-item-ts" src={typescriptIcon} alt="typescript" />
             <img className="icon-item" src={reactIcon} alt="reactjs" />
-            <img className="icon-item-ex" src={mongodbIcon} alt="mongodb" />
-            <img className="icon-item-ex" src={expressIcon} alt="expressjs" />
-            <img className="icon-item-nd" src={nodeIcon} alt="nodejs" />
-            <img className="icon-item-ex" src={firebaseIcon} alt="firebase" />
-            <img className="icon-item" src={dockerIcon} alt="docker" />
-            <img className="icon-item" src={codeIcon} alt="code" />
+            <img className="icon-item" src={mongodbIcon} alt="mongodb" />
+            <img className="icon-item" src={nodeIcon} alt="nodejs" />
+            <img className="icon-item" src={expressIcon} alt="expressjs" />
+            <img className="icon-item" src={typescriptIcon} alt="typescript" />
+            <img className="icon-item" src={firebaseIcon} alt="firebase" />
+            <img className="icon-item" src={codeCloseIcon} alt="code" />
         </div>
 
     )
