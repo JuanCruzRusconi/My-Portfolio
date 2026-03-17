fb984794322d14ff99b9faa5cffd10ee84df4674
JuanCruzRusconi <juanrusconi@hotmail.es>
2026-03-17 13:11:42 -0300

    refactor: simplify language provider logic

--- src/context/langContext.jsx
+++ src/context/langContext.jsx
@@ -5,44 +5,27 @@ import spanishMessages from '../lang/es-es.json';
 
 const langContext = React.createContext();
 
-const LangProvider = ({children}) => {
+const messagesMap = {
+    "en-us": englishMessages,
+    "es-es": spanishMessages,
+};
 
-    let defaultLocale = "es-es";
-    let defaultMessages = spanishMessages;
-    
-    const lang = localStorage.getItem('lang');
-    if(lang) {
-        defaultLocale = lang;
-        if(lang === 'en-us') defaultMessages = englishMessages;
-        else defaultMessages = spanishMessages;
-    };
+const LangProvider = ({ children }) => {
 
-    const [ messages, stablishMessages ] = useState(defaultMessages);
+    const savedLang = localStorage.getItem("lang") || "es-es";
 
-    const [ locale, stablishLocale ] = useState(defaultLocale);
+    const [ locale, setLocale ] = useState(savedLang);
+    const [ messages, setMessages ] = useState(messagesMap[savedLang]);
 
     const handleLanguaje = (lan) => {
-        
-        switch (lan) {
-            case 'en-us':
-                stablishMessages(englishMessages);
-                stablishLocale('en-us');
-                localStorage.setItem('lang', 'en-us');
-                break;
-            case 'es-es':
-                stablishMessages(spanishMessages);
-                stablishLocale('es-es');
-                localStorage.setItem('lang', 'es-es');
-                break;
-            default:
-                stablishMessages(spanishMessages);
-                stablishLocale('es-es');
-        }
+        setLocale(lan);
+        setMessages(messagesMap[lan]);
+        localStorage.setItem("lang", lan);
     };
 
         return (
             
-            <langContext.Provider value={{handleLanguaje: handleLanguaje, locale}}>
+            <langContext.Provider value={{ handleLanguaje: handleLanguaje, locale }}>
                 <IntlProvider locale={locale} messages={messages}>
                     {children}
                 </IntlProvider>
